2b34f6ee86a8dc3b2b4b306b8cac471aed4c6336
Ubuntu <ubuntu@ip-172-31-88-106.ec2.internal>
2021-12-14 17:16:22 +0000

    Added *.swift

---
+++ main.swift
@@ -0,0 +1,49 @@
+//
+//  StackExample.swift
+//
+//  Created by Jenoe Balote
+//  Created on 2021-12-12
+//  Version 1.0
+//  Copyright (c) 2021 Jenoe Balote. All rights reserved.
+//
+//  This program demonstrates a stack.
+//
+
+import Foundation
+
+// Takes user input in a loop and permits the user to make multiple commands
+// exit = end of program
+let aStack = MrCoxallStack<Int>()
+
+let allowedCommands = ["push", "pop", "show", "exit"]
+print("Acceptable Commands:\npush <number> - pull - show - exit")
+
+while true {
+    print("> ", terminator: "")
+    let userInput = readLine()
+
+    if userInput != nil {
+        if userInput!.starts(with: allowedCommands[0]) {
+            let userCommandArray = userInput!.components(separatedBy: " ")
+            if let userNumber = Int(userCommandArray[1]) {
+                aStack.push(pushElement: userNumber)
+            } else {
+                print("Please enter a valid command.")
+            }
+        } else if userInput!.starts(with: allowedCommands[1]) {
+            do {
+                try print("Removed: \(aStack.pop())")
+            } catch {
+                print("The stack is empty.")
+            }
+        } else if userInput!.starts(with: allowedCommands[2]) {
+            aStack.showStack()
+        } else if userInput!.starts(with: allowedCommands[3]) {
+            break
+        } else {
+            print("Please enter a valid command.")
+        }
+    }
+}
+
+print("\nDone.")
